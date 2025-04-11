55724d3dba11354fcfb113e84da957ae37d24b8a
Maria Eva Modarelli <modarellimariaeva@gmail.com>
2025-04-11 17:28:48 -0300

    agregando la función para comparar textos y el input con el loop

--- src/palindrome.py
+++ src/palindrome.py
@@ -21,3 +21,25 @@ def clean_text(text):
 
     return cleaned
 
+def compare_characters(cleaned_text):
+    
+    return cleaned_text == cleaned_text[::-1]
+
+if __name__ == "__main__":
+    while True:
+        
+        frase = input("Ingrese una palabra o frase (o 'salir' para terminar): ")
+        
+        if frase.lower() == 'salir':  
+            print("¡Hasta luego!")
+            break
+
+        resultado = is_palindrome(frase)
+        
+        if resultado == "No se permiten números":
+            print("¡Error! No se permiten números.")
+        else:
+            if resultado:
+                print(f'"{frase}" es un palíndromo')
+            else:
+                print(f'"{frase}" no es un palíndromo')
